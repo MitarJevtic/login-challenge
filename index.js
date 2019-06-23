c347b68abe547baeafa82ba77156862c4c20ecc3
Mitar <mitarjevtic@hotmail.com>
2019-06-24 00:09:28 +0200

    pocetak

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/index.html'));
+});
+app.listen(3000);
+console.log("Server running at http://localhost:3000/");
